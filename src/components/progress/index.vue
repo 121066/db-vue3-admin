0e7a41d306b2d8b739f61a69ac3b8c4dd8cbfdaf
十一 <1318720440@qq.com>
2023-07-26 21:55:45 +0800

    fix 添加进度

---
+++ src/components/progress/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="progress"></div>
+</template>
+<script setup name="progress">
+const props = defineProps({
+    num: {
+        type: Number,
+        default: 0,
+    },
+});
+</script>
+<style lang="scss" scoped>
+.progress {
+    width: 100%;
+}
+</style>
