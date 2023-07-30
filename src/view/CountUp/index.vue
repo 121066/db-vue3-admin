e1ab9cc972e6e5b07704ee45cf247e099474f9f5
十一 <1318720440@qq.com>
2023-07-30 21:12:28 +0800

    fix: 添加up函数

--- src/view/CountUp/index.vue
+++ src/view/CountUp/index.vue
@@ -1,3 +1,31 @@
 <template>
     <div>数字增加</div>
 </template>
+<script setup>
+const handleCount = () => {
+    const ctx = document.querySelector('.num');
+    animation(500, 0, 500, (val) => {
+        ctx.textContent = val;
+    });
+    function animation(duration, from, to, onProgress) {
+        const dis = to - from;
+        const speed = dis / duration;
+        const startTime = Date.now();
+        let value = to;
+        function _run() {
+            const now = Date.now();
+            const time = now - startTime;
+            if (time >= duration) {
+                value = to;
+                onProgress(value);
+                return;
+            }
+            const d = time * speed;
+            value = from + d;
+            onProgress(value);
+            requestAnimationFrame(_run);
+        }
+        requestAnimationFrame(_run);
+    }
+};
+</script>
