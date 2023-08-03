647bf4394c6511cd5b469671b0140ad1e02174eb
十一 <1318720440@qq.com>
2023-08-03 20:14:19 +0800

    fix: 添加输入搜索模板

--- src/view/Labels/index.vue
+++ src/view/Labels/index.vue
@@ -9,12 +9,68 @@
         >
             {{ tag.name }}
         </el-tag>
+        <div class="search">
+            <span class="search_label">搜索</span>
+            <!-- 搜索区域 -->
+            <el-input
+                placeholder="请输入关键字搜索"
+                v-model="int.search"
+                @focus="intFoucs"
+                @blur="intBlur"
+                :fetch-suggestions="querySearchAsync"
+                @change="intChange"
+                class="search_int"
+            ></el-input>
+            <!-- 下拉面板区域 -->
+            <div class="search_list" v-if="searchFlag"></div>
+        </div>
     </div>
 </template>
 <script setup>
-import { reactive } from 'vue';
-
+import { reactive, ref } from 'vue';
+const int = reactive({
+  search: '搜索',
+  searchList: ''
+})
+const searchFlag = ref(false)
 const data = reactive({
-    tags: [{ id: 1, name: '标签', disabled: false }],
+  tags: [{ id: 1, name: '标签', disabled: false }],
 });
+const intFoucs = () => {
+  searchFlag.value = true
+}
+const intBlur = () => {
+  searchFlag.value = false
+}
+const querySearchAsync = (e) => {
+  console.log(e, 'eee')
+}
+const intChange = (e) => {
+  console.log(e, 'shus')
+}
 </script>
+<style lang="scss" scoped>
+.search {
+    margin: 16px;
+    display: flex;
+    flex-direction: row;
+    width: 400px;
+    justify-content: flex-start;
+    align-items: center;
+    position: relative;
+    transition: all 0.5s ease-in;
+    .search_label {
+        min-width: 40px;
+    }
+    .search_int {
+    }
+    .search_list {
+        position: absolute;
+        top: 40px;
+        left: 0;
+        height: 400px;
+        background: red;
+        width: 100%;
+    }
+}
+</style>
