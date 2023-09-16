0b5d0e914795fc4c8b2e2ee6a00e87b154aed267
十一 <1318720440@qq.com>
2023-09-16 23:41:41 +0800

    fix: 添加全屏效果

---
+++ src/components/screenfull/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <span @click="onclickFull">全屏</span>
+</template>
+<script setup>
+import screenfull from 'screenfull';
+import { onMounted } from 'vue';
+const init = () => {
+  if (screenfull.enabled) {
+    screenfull.on('change', change)
+  }
+}
+const isFullscreen = ref(false)
+const onclickFull = () => {
+  if (!screenfull.enabled) {
+    console.log('全屏')
+    return false
+  }
+  screenfull.toggle()
+}
+const change = () => {
+  isFullscreen = screenfull.isFullscreen
+}
+onMounted(() => {
+  init()
+})
+</script>
--- src/layout/header/header.vue
+++ src/layout/header/header.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="header">
-    <div class="header_left">后台管理系统</div>
-    <div class="header_right">退出</div>
-  </div>
+    <div class="header">
+        <div class="header_left">组件测试系统</div>
+        <div class="header_right">
+            <el-dropdown>
+                <el-button type="text" class="btn"> 设置 </el-button>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>
+                            <screenfull />
+                        </el-dropdown-item>
+                        <el-dropdown-item>Action 2</el-dropdown-item>
+                        <el-dropdown-item>Action 3</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+            <el-button class="btn" type="text">退出</el-button>
+        </div>
+    </div>
 </template>
+<script setup>
+import screenfull from "@/components/screenfull/index.vue";
+
+</script>
 <style lang="scss">
-  .header{
+.header {
     padding: 5px 10px;
     height: 100%;
     display: flex;
@@ -13,19 +31,40 @@
     justify-content: space-between;
     background-color: var(--theme-color);
     align-items: center;
-    .header_left{
-      display: inline-block;
-    margin: 0;
-    color: #fff;
-    font-weight: 400;
-    line-height: 50px;
-    font-size: 28px;
-    font-family: alihan;
-    vertical-align: middle;
+    .header_left {
+        display: inline-block;
+        margin: 0;
+        color: #fff;
+        font-weight: 400;
+        line-height: 50px;
+        font-size: 28px;
+        font-family: alihan;
+        vertical-align: middle;
     }
-    .header_right{
-      color: #fff;
-      font-size: 18px;
+    .header_right {
+        color: #fff;
+        font-size: 18px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        line-height: 32px;
+        .btn {
+            color: #fff;
+            margin: 0 5px;
+        }
     }
-  }
+}
+.example-showcase .el-dropdown-link {
+    cursor: pointer;
+    color: var(--el-color-primary);
+    display: flex;
+    align-items: center;
+}
+.el-dropdown {
+    line-height: 3;
+}
+::v-deep .el-dropdown:focus-visible {
+    outline: none !important;
+}
 </style>
