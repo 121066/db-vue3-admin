d7c0181c9d1a873ce18d8131de4df2258b618dac
121066 <1318720440@qq.com>
2023-11-30 22:15:31 +0800

    fix: 添加字典校验

--- src/view/Dictionary/components/dictForm.vue
+++ src/view/Dictionary/components/dictForm.vue
@@ -1,51 +1,30 @@
 <template>
     <div>
-        <el-dialog
-            v-model="centerDialogVisible"
-            title="新增"
-            width="50%"
-            align-center
-            @close="close"
-        >
+        <el-dialog v-model="centerDialogVisible" title="新增" width="50%" align-center @close="close">
             <div>
-                <el-form :model="addForm" ref="addFroms">
+                <el-form :model="addForm" ref="addFroms" :rules="rules">
                     <el-row :gutter="24">
                         <el-col :span="12">
                             <el-form-item label="字典名称" prop="label">
-                                <el-input
-                                    v-model="addForm.label"
-                                    placeholder="请输字典名称"
-                                ></el-input>
+                                <el-input v-model="addForm.label" placeholder="请输字典名称"></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :span="12">
                             <el-form-item label="字典key" prop="key">
-                                <el-input
-                                    v-model="addForm.key"
-                                    placeholder="请输入字典key"
-                                ></el-input>
+                                <el-input v-model="addForm.key" placeholder="请输入字典key"></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :span="12">
                             <el-form-item label="排序顺序" prop="desc">
-                                <el-input
-                                    v-model.number="addForm.desc"
-                                    placeholder="请输入排序顺序"
-                                ></el-input>
+                                <el-input v-model.number="addForm.desc" placeholder="请输入排序顺序"></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :span="12">
                             <el-form-item label="是否禁用" prop="flag">
-                                <el-radio-group
-                                    v-model="addForm.flag"
-                                    class="ml-4"
-                                >
+                                <el-radio-group v-model="addForm.flag" class="ml-4">
                                     <el-radio :label="false" size="large">
-                                        启用</el-radio
-                                    >
-                                    <el-radio :label="true" size="large"
-                                        >禁用</el-radio
-                                    >
+                                        启用</el-radio>
+                                    <el-radio :label="true" size="large">禁用</el-radio>
                                 </el-radio-group>
                             </el-form-item>
                         </el-col>
@@ -69,49 +48,54 @@ import { reactive } from "vue";
 // import { storeToRefs } from 'pinia'
 const store = useDictionary()
 const props = defineProps({
-  modelValue: {
-    type: Boolean,
-    default: false
-  }
+    modelValue: {
+        type: Boolean,
+        default: false
+    }
 })
 const addForm = reactive({
-  key: '',
-  label: '',
-  desc: '',
-  flag: false,
-  list: []
+    key: '',
+    label: '',
+    desc: '',
+    flag: false,
+    list: []
 })
 const rules = {
-  key: [{ required: true, message: '请输入字典key', target: 'blur' }],
-  label: [{ required: true, message: '请输入字典名称', target: 'blur' }],
-  desc: [{ required: false, message: '' }],
-  flag: [{ required: false }]
+    key: [{ required: true, message: '请输入字典key', target: 'blur' }],
+    label: [{ required: true, message: '请输入字典名称', target: 'blur' }],
+    desc: [{ required: false, message: '' }],
+    flag: [{ required: false }]
 
 }
 const addFroms = ref(null)
 const emit = defineEmits(['update:modelValue'])
 const centerDialogVisible = computed({
-  get () {
-    return props.modelValue
-  },
-  set (val) {
-    emit('update:modelValue', val)
-  }
+    get() {
+        return props.modelValue
+    },
+    set(val) {
+        emit('update:modelValue', val)
+    }
 })
 const addRef = (value) => {
-  //   console.log(value, '我是父组件传值过来的')
+    //   console.log(value, '我是父组件传值过来的')
 }
 const close = () => {
-  emit('update:modelValue', false)
-  addFroms.value.resetFields()
+    emit('update:modelValue', false)
+    addFroms.value.resetFields()
 }
 // 确认
 const addConfirm = () => {
-  store.setDictContent(addForm, addForm.key)
-  close()
+    addFroms.value.validate((valid) => {
+        if (valid) {
+            store.setDictContent(addForm, addForm.key)
+            close()
+        }
+    })
+
 }
 defineExpose({
-  centerDialogVisible,
-  addRef
+    centerDialogVisible,
+    addRef
 })
 </script>
